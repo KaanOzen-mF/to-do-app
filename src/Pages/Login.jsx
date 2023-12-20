37b2757b53d954fdbcfb3422e1a0c0fb4ff60a08
KaanOzen-mF <kaanozn98@gmail.com>
2023-12-20 14:25:52 +0300

    Add login page

---
+++ src/Pages/Login.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import SignIn from "../components/SignIn";
+import SignUp from "../components/SignUp";
+
+export default function Login() {
+  return (
+    <div>
+      <SignIn />
+      <SignUp />
+    </div>
+  );
+}
