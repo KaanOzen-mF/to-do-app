805d46932397deb0e6b2cfc831ec51fdec90c997
KaanOzen-mF <kaanozn98@gmail.com>
2023-12-20 15:56:23 +0300

    login page change view btn add

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,12 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import SignIn from "../components/SignIn";
 import SignUp from "../components/SignUp";
 
 export default function Login() {
+  const [activeView, setActiveView] = useState("signIn");
+
   return (
     <div>
-      <SignIn />
-      <SignUp />
+      <h1>Login Page</h1>
+      <button onClick={() => setActiveView("signIn")}>Sign In</button>
+      <button onClick={() => setActiveView("signUp")}>Sign Up</button>
+
+      {activeView === "signIn" ? <SignIn /> : <SignUp />}
     </div>
   );
 }
