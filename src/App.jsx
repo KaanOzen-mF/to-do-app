9acba3a419be549dc14f9d5374db25a304d1d0ec
KaanOzen-mF <kaanozn98@gmail.com>
2023-12-21 17:59:06 +0300

    routes added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./Pages/Login";
+import Todo from "./Pages/Todo";
 
 export default function App() {
   return (
-    <>
-      <Login />
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/todo" element={<Todo />} />
+        {/* other routes */}
+      </Routes>
+    </Router>
   );
 }
