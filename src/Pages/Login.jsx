a567971c35b5a3ac4eac300424159a6ae20182aa
KaanOzen-mF <kaanozn98@gmail.com>
2023-12-21 17:58:33 +0300

    login page done and Todo page logput functionality done

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -5,13 +5,21 @@ import SignUp from "../components/SignUp";
 export default function Login() {
   const [activeView, setActiveView] = useState("signIn");
 
+  const handleSignUpSuccess = () => {
+    setActiveView("signIn");
+  };
+
   return (
     <div>
       <h1>Login Page</h1>
       <button onClick={() => setActiveView("signIn")}>Sign In</button>
       <button onClick={() => setActiveView("signUp")}>Sign Up</button>
 
-      {activeView === "signIn" ? <SignIn /> : <SignUp />}
+      {activeView === "signIn" ? (
+        <SignIn />
+      ) : (
+        <SignUp onSignUpSuccess={handleSignUpSuccess} />
+      )}
     </div>
   );
 }
---
+++ src/Pages/Todo.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import LogoutButton from "../components/Logout";
+
+export default function Todo() {
+  return (
+    <>
+      <h1>Todo Page</h1>
+      <LogoutButton />
+    </>
+  );
+}
