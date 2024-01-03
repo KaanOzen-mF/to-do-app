877af280351bd9de4eab51f15cb2a67adf51b82f
KaanOzen-mF <kaanozn98@gmail.com>
2024-01-03 10:15:41 +0300

    Logout compoent comment line add

--- src/components/Logout.jsx
+++ src/components/Logout.jsx
@@ -1,21 +1,25 @@
 import React from "react";
 import { signOut } from "firebase/auth";
-import { auth } from "../firebase"; // Adjust this path as necessary
+import { auth } from "../firebase";
 import { useNavigate } from "react-router-dom";
 
+// LogoutButton is a functional component that renders a logout button
 export default function LogoutButton() {
-  const navigate = useNavigate();
+  const navigate = useNavigate(); // useNavigate hook for programmatically navigating
+
+  // handleLogout is an asynchronous function to handle the logout process
   const handleLogout = async () => {
     try {
-      await signOut(auth);
+      await signOut(auth); // Attempt to sign out using Firebase authentication
       console.log("Logged out successfully!");
-      navigate("/");
+      navigate("/"); // Redirect to the homepage after successful logout
     } catch (error) {
-      console.error("Logout failed: ", error);
+      console.error("Logout failed: ", error); // Log error if logout fails
     }
   };
 
   return (
+    // Render a button that, when clicked, triggers the handleLogout function
     <button onClick={handleLogout} className="logout_btn_container">
       Logout
     </button>
