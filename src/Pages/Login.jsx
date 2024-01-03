35340f670ba305e3de4d75cd3916439f99882de3
KaanOzen-mF <kaanozn98@gmail.com>
2024-01-03 10:33:25 +0300

    Login page comment lines add

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -4,18 +4,23 @@ import SignUp from "../components/SignUp";
 import "../Pages.css";
 import "../Components.css";
 
+// Login component manages the authentication views for SignIn and SignUp
 export default function Login() {
+  // State to toggle between SignIn and SignUp views
   const [activeView, setActiveView] = useState("signIn");
-
+  // Function to handle successful sign-up and switch back to sign-in view
   const handleSignUpSuccess = () => {
     setActiveView("signIn");
   };
-
+  // Rendering the component
   return (
     <div className="login_page">
+      {/* Conditional rendering based on the activeView state */}
       {activeView === "signIn" ? (
+        // Renders the SignIn component if activeView is 'signIn'
         <SignIn setActiveView={setActiveView} />
       ) : (
+        // Renders the SignUp component if activeView is not 'signIn'
         <SignUp
           setActiveView={setActiveView}
           onSignUpSuccess={handleSignUpSuccess}
